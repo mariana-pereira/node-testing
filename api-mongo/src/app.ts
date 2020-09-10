75fbc381aa237bca40ee76f137ce6a19d4202e7f
Mariana Pereira <marianna.22sp@gmail.com>
2020-09-10 15:53:05 -0300

    Configure project

---
+++ api-mongo/src/app.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import routes from './routes';
+
+class AppController {
+  public express: express.Application;
+
+  constructor() {
+    this.express = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.express.use(express.json());
+  }
+
+  routes() {
+    this.express.use(routes);
+  }
+}
+
+export default new AppController().express;
